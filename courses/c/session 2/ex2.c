0be621dee1eb24f5e928462ab7b68bd4a8c31dc3
Hf Wassim <56920664+hfwassim010@users.noreply.github.com>
2024-11-09 13:34:43 +0100

    Update ex2.c

--- courses/c/session 2/ex2.c
+++ courses/c/session 2/ex2.c
@@ -1,59 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int saisir(){
+int saisir() {
     int n;
-    int test = 0;
-    while(test == 0) {
-        printf("saisir N>0 et n<=6 : ");
-        scanf("%d",&n);
-        test = n >=1 && n <=6;
-    }
+    do {
+        printf("Saisir N (0 < N <= 6): ");
+        scanf("%d", &n);
+    } while (n <= 0 || n > 6);  // Corrected range check
 
     return n;
-
 }
 
+void remplir(int a[10], int b[10], int n) {
+    for (int i = 0; i < n; i++) {
+        do {
+            printf("Saisir A[%d]: ", i);
+            scanf("%d", &a[i]);  // Use &a[i] to store the input correctly
+        } while (a[i] < 0);  // Check that a[i] is non-negative
 
-
-int pair(int n) {
-    return n%2 == 0;
+        do {
+            printf("Saisir B[%d]: ", i);
+            scanf("%d", &b[i]);  // Use &b[i] to store the input correctly
+        } while (b[i] < 0);  // Check that b[i] is non-negative
+    }
 }
 
-int somme_div(int n) {
-    int s = 1;
-    for (int i=2;i<=n/2;i++) {
-        if (i%n==0){
-            s++;
-        }
+int Somme(int a[10], int b[10], int n) {
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        s += a[i] * b[i];
     }
     return s;
 }
 
-int fact(int n) {
-    int f = 1;
-    for (int i=1;i<=n;i++) {
-        f*=i;
-    }
-    return f;
-}
+int main() {
+    int A[10];
+    int B[10];
 
-
-
-int main()
-{
     int n = saisir();
-    printf("N est :%d \n",n);
-
-    if(pair(n)){
-        printf("N est pair !\n");
-    }else {
-        printf("N est impair !\n");
-    }
-
-    printf("somme des diviseurs de N est : %d \n",somme_div(n));
-    printf("le factoriel de N est : %d \n",fact(n));
-
+    remplir(A, B, n);
+    int s = Somme(A, B, n);
+    printf("La somme des produits A[i] * B[i] est: %d\n", s);  // Print the result
     return 0;
 }
